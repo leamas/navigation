9fd3d6ef85eac004c26a8e42d532c0816a58db0d
Alec Leamas <leamas.alec@gmail.com>
2017-02-22 16:42:17 +0100

    chart: claenup, use background to optimize

--- charts/static/chart.js
+++ charts/static/chart.js
@@ -10,10 +10,6 @@ function onBodyLoad() {
 
     const HANDLE_WIDTH = 100;
 
-    /** Max length of click to not be a pull (ms). */
-    const CLICK_THRESHOLD = 200;
-
-    /** List of all user-defined waypoints. */
     var triangle = {'x': 2225, 'y': 1300};
     var ruler = {'x': 2225, 'y': 500};
     var currentTool = null;
@@ -39,60 +35,6 @@ function onBodyLoad() {
         };
     }
 
-    /** Draw the X marker for a user click/waypoint. */
-    function drawMarker(p, canvas) {
-        var image = new Image();
-        image.src = MARKER_SRC;
-        var ctx = canvas.getContext('2d')
-        ctx.drawImage(image, p.x - MARKER_WIDTH, p.y - MARKER_WIDTH);
-    }
-
-    /** Draw lines connecting the global waypoints. */
-    function drawLines(canvas) {
-        if (waypoints.length  < 2)
-            return;
-        var ctx = canvas.getContext('2d')
-        ctx.beginPath();
-        ctx.moveTo(waypoints[0].x, waypoints[0].y);
-        for (var i = 1; i < waypoints.length; i += 1)
-            ctx.lineTo(waypoints[i].x, waypoints[i].y);
-        ctx.stroke();
-    }
-
-    /** Redraw image, markers and lines. */
-    function redraw(canvas) {
-        var ctx = canvas.getContext('2d')
-        var image = new Image();
-        image.src = MAP_SOURCE;
-        ctx.drawImage(image, 0, 0);
-        drawRuler(ruler);
-        drawTriangle(triangle);
-    }
-
-    /**
-     * Check if p is "close" to an existing waypoint; if so, remove it.
-     * Return true if a waypoint is removed.
-     */
-    function checkRemove(p, canvas) {
-        for (var i = 0; i < waypoints.length; i += 1) {
-            if (distance(p, waypoints[i]) < MARKER_WIDTH) {
-                waypoints.splice(i, 1);
-                redraw(canvas);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /** Return index in waypoints to an element "close" to p or -1. */
-    function findNearbyTool(p, canvas) {
-        if (distance(p, triangle) < HANDLE_WIDTH)
-            return 'triangle';
-        if (distance(p, ruler) < HANDLE_WIDTH)
-            return 'ruler';
-        return null;
-    }
-
     function drawRuler(p) {
         var canvas = document.getElementById('mapCanvas');
         var ctx = canvas.getContext('2d')
@@ -109,6 +51,24 @@ function onBodyLoad() {
         ctx.drawImage(image, p.x - TRIANGLE_WIDTH/2, p.y - TRIANGLE_HEIGHT/2);
     }
 
+
+    /** Redraw image, ruler and triangle. */
+    function redraw(canvas) {
+        var ctx = canvas.getContext('2d')
+        var image = new Image();
+        image.src = MAP_SOURCE;
+        ctx.drawImage(image, 0, 0);
+        drawRuler(ruler);
+        drawTriangle(triangle);
+    }
+
+    function findNearbyTool(p, canvas) {
+        if (distance(p, triangle) < HANDLE_WIDTH)
+            return 'triangle';
+        if (distance(p, ruler) < HANDLE_WIDTH)
+            return 'ruler';
+        return null;
+    }
 
     /** DOM mousedown event: initiate currentWaypoint and mousedownAt. */
     function onMousedown(e) {
@@ -150,13 +110,10 @@ function onBodyLoad() {
         function setupImages(onLoad) {
             images.push(new Image());
             images.push(new Image());
-            images.push(new Image());
-            images[0].src = MAP_SOURCE
-            images[1].src = TRIANGLE_SRC;
-            images[2].src = RULER_SRC;
+            images[0].src = TRIANGLE_SRC;
+            images[1].src = RULER_SRC;
             images[0].onload = onLoad;
             images[1].onload = onLoad;
-            images[2].onload = onLoad;
         }
 
 
@@ -165,10 +122,9 @@ function onBodyLoad() {
             //if (imagesCount >= 0)
             //    return;
             var canvas = document.getElementById('mapCanvas');
-            ctx.drawImage(images[0], 0, 0);
-            ctx.drawImage(images[1], triangle.x - TRIANGLE_WIDTH/2,
+            ctx.drawImage(images[0], triangle.x - TRIANGLE_WIDTH/2,
                 triangle.y - TRIANGLE_HEIGHT/2);
-            ctx.drawImage(images[2], ruler.x - RULER_WIDTH/2,
+            ctx.drawImage(images[1], ruler.x - RULER_WIDTH/2,
                 ruler.y - RULER_HEIGHT/2);
         }
 
@@ -179,7 +135,6 @@ function onBodyLoad() {
         setupImages(loadImages);
         imagesCount = images.length;
     }
-
 
     window.addEventListener('mousedown', onMousedown);
     window.addEventListener('mouseup', onMouseup);
